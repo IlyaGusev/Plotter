f647c00ce936cb3a1c5048dc13f8d7f1e72f7441
mikhaylova-daria <darya.mikhailova@phystech.edu>
2015-10-04 22:45:26 +0300

    корень; есть баг в центрировании, не могу пока поймать

--- EquationEditor/Model/RadicalControlModel.cpp
+++ EquationEditor/Model/RadicalControlModel.cpp
@@ -8,12 +8,13 @@
 CRadicalControlModel::CRadicalControlModel(CRect rect, std::weak_ptr<IBaseExprModel> parent) {
 	this->parent = parent;
 	this->rect = rect;
+
 }
 
 void CRadicalControlModel::Resize()
 {
-	int width = firstChild->GetRect().GetWidth() + secondChild->GetRect().GetWidth() + 5; //+5 для знака дроби
-	int height = MAX(secondChild->GetRect().GetHeight(), firstChild->GetRect().GetHeight() + 5); // +5 для знака корня
+	int width = firstChild->GetRect().GetWidth() + secondChild->GetRect().GetWidth() + 15; //+15 для знака дроби
+	int height = MAX(secondChild->GetRect().GetHeight(), firstChild->GetRect().GetHeight() + 0.5 * secondChild->GetRect().GetHeight()) + 15; 
 
 	rect.Right() = rect.Left() + width;
 	rect.Bottom() = rect.Top() + height;
@@ -25,15 +26,15 @@ void CRadicalControlModel::PlaceChildren()
 
 	CRect oldRect = firstChild->GetRect();
 	newRect.Top() = rect.Top();
-	newRect.Bottom() = rect.Bottom() - 5;
+	newRect.Bottom() = rect.Bottom() - 0.5 * secondChild->GetRect().GetHeight();
 	newRect.Left() = rect.Left();
 	newRect.Right() = rect.Left() + oldRect.GetWidth();
 	firstChild->SetRect(newRect);
 	
 	oldRect = secondChild->GetRect();
 	newRect.Bottom() = rect.Bottom();
-	newRect.Top() = rect.Bottom() - oldRect.GetHeight();
-	newRect.Left() = firstChild->GetRect().Right() + 5;
+	newRect.Top() = rect.Top() + 10;//rect.Bottom() - oldRect.GetHeight();
+	newRect.Left() = firstChild->GetRect().Right() + 15;
 	newRect.Right() = newRect.Left() + oldRect.GetWidth();
 	secondChild->SetRect(newRect);
 
@@ -54,7 +55,7 @@ void CRadicalControlModel::InitializeChildren()
 	secondChild = std::make_shared<CExprControlModel>(CExprControlModel(childRect, std::weak_ptr<IBaseExprModel>(shared_from_this())));
 	secondChild->InitializeChildren();
 
-	CRect newRect = CRect(rect.Left(), rect.Top(), rect.Left()+ 2*childRect.GetWidth() + 5, rect.Top() + childRect.GetHeight() + 5);
+	CRect newRect = CRect(rect.Left(), rect.Top(), rect.Left()+ 2*childRect.GetWidth() + 15, rect.Top() + childRect.GetHeight());
 	SetRect(newRect);
 	PlaceChildren();
 }
@@ -65,6 +66,10 @@ std::list<std::shared_ptr<IBaseExprModel>> CRadicalControlModel::GetChildren() c
 
 void CRadicalControlModel::SetRect(CRect rect) {
 	this->rect = rect;
+	this->params.polygon.clear();
+	this->params.polygon.push_back(CLine(firstChild->GetRect().Right() + 15, rect.Top(), rect.Right(), rect.Top()));
+	this->params.polygon.push_back(CLine(firstChild->GetRect().Right() - 10, rect.Bottom() - 0.5 * rect.GetHeight(), firstChild->GetRect().Right(), rect.Bottom()));
+	this->params.polygon.push_back(CLine(firstChild->GetRect().Right(), rect.Bottom(), firstChild->GetRect().Right() + 15, rect.Top()));
 }
 
 ViewType CRadicalControlModel::GetType() const {
@@ -73,7 +78,9 @@ ViewType CRadicalControlModel::GetType() const {
 
 void CRadicalControlModel::MoveBy(int dx, int dy) {
 	rect.MoveBy(dx, dy);
-	params.polygon.front().MoveBy(dx, dy);
+	for (CLine line : this->params.polygon) {
+		line.MoveBy(dx, dy);
+	}
 }
 
 void CRadicalControlModel::GoLeft(std::shared_ptr<const IBaseExprModel> from, CCaret& caret) const {

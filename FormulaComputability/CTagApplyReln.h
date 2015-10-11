35ece4cbc397acd03cd04866ffddef6d68db8d20
Nikolai <nvvaulin@gmail.com>
2015-10-11 22:12:09 +0300

    add files

---
+++ FormulaComputability/CTagApplyReln.h
@@ -0,0 +1,75 @@
+#pragma once
+#ifndef _TAGAPPLY_H
+#define _TAGAPPLY_H
+#include "Tag.h"
+#include "TagCi.h"
+
+
+//both Apply (= CTagApplyReln<NUMBER>) and Reld (= CTagApplyReln<BOOL>)
+template<CType TFunc>
+class CTagApplyReln : public CTagAtamar
+{
+public:
+	CTagApplyReln();
+	virtual void operator ()(const CNode& node)const;
+protected:
+	static void enterToAllLimitableArgs(const CNode& node);
+};
+
+
+template<CType TFunc>
+CTagApplyReln<TFunc>::CTagApplyReln()
+{
+	type = TFunc | SPECIAL;
+}
+
+template<CType TFunc>
+void CTagApplyReln<TFunc>::operator ()(const CNode& node)const
+{
+	hasNoAttributes(node);
+	hasNoText(node);
+
+
+	auto child = node.first_child();
+	const CTag& func = CTagContainer::getTag(child.name());
+	if (!((func.type & CALCULATEBLE) && (func.type & TFunc))) {
+		throwException(node, child.offset_debug(), INVALID_ARGUMENT);
+	}
+	child = func.checkSignature(child);
+	if (!(child.empty())) {
+		throwException(node, child.offset_debug(), INVALID_ARGUMENT);
+	}
+	if (func.getType() & LIMITABLE) {
+		enterToAllLimitableArgs(node);
+	}
+	else {
+
+		enterToAllChilds(node);
+	}
+}
+
+template<CType TFunc>
+void CTagApplyReln<TFunc>::enterToAllLimitableArgs(const CNode& node)
+{
+	//предполагаем, что все теги корректно заданы
+
+
+	CNode child = node.first_child();
+
+	CNode bVar = child.next_sibling().first_child();
+	while (!child.empty()) {
+		CTag& childTag = CTagContainer::getTag(child.name());
+		if (childTag.getType() & CONDITION) {
+			CTagCi::AddIdentifier(bVar, BOUND | VARIABLE | TFunc);
+		}
+		childTag(child);
+		if (childTag.getType() & LIMIT_UP) {
+			CTagCi::AddIdentifier(bVar, BOUND | VARIABLE | TFunc);
+		}
+		child = child.next_sibling();
+	}
+	CTagCi::deleteIdentifier(bVar.text().as_string());
+}
+
+
+#endif
---
+++ FormulaComputability/TagCn.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef _TacCn_H
+#define _TacCn_H
+#include "Tag.h"
+
+class CTagCn : public CTagAtamar
+{
+	void nodeIsInteger(const CNode& node)const;
+	void nodeIsReal(const CNode& node)const;
+public:
+	CTagCn();
+	virtual void operator ()(const CNode& node)const;
+};
+
+#endif

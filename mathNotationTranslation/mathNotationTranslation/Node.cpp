b3c31577c2c4f9bbc4d2632d2fd3894c60236a3e
Ilya Gusev <ilya.gusev@phystech.edu>
2015-10-12 10:39:03 +0300

    cpp and h files separating, const, public, private qulifiers

---
+++ mathNotationTranslation/mathNotationTranslation/Node.cpp
@@ -0,0 +1,139 @@
+#include "Node.h"
+
+string Node::translate(int notation) const{
+	return string();
+}
+map<string, array<string, 3>> Node::createMap()
+{
+	map<string, array<string, 3>> m;
+	m["+"] = { " <mo> + </mo> ", "", " + " };
+	m["-"] = { " <mo> - </mo> ", "", " - " };
+	m["*"] = { " <mo> * </mo> ", "", " * " };
+	m["/"] = { " <mo> / </mo> ", "", " / " };
+	m["="] = { " <mo> = </mo> ", "", " = " };
+	return m;
+}
+void Node::setFence(string _lfence, string _rfence){
+	lfence = _lfence;
+	rfence = _rfence;
+}
+string Node::addFence(int notation, string _s) const{
+	string s = _s;
+	switch (notation) {
+		case MATHML:
+			if (lfence == "(")
+				s = "<mfenced>" + s + "</mfenced>";
+			break;
+		case OPENMATH:
+			s = "";
+			break;
+		case TEX:
+			s = lfence + s + rfence;
+			break;
+	}
+	return s;
+}
+
+
+NumNode::NumNode(float _number): number(_number) {}
+NumNode::~NumNode(){}
+string NumNode::translate(int notation) const{
+	string result;
+	string num = string(to_string(number));
+	switch (notation) {
+		case MATHML:
+			result = " <mn> " + num + " </mn> ";
+			break;
+		case OPENMATH:
+			result = num;
+			break;
+		case TEX:
+			result = num;
+			break;
+	}
+	return addFence(notation, result);
+}
+
+IdNode::IdNode(string _id): id(_id) {}
+IdNode::~IdNode(){}
+string IdNode::translate(int notation) const{
+	string result;
+	switch (notation) {
+		case MATHML:
+			result = " <mi> " + id + " </mi> ";
+			break;
+		case OPENMATH:
+			result = id;
+			break;
+		case TEX:
+			result = id;
+			break;
+	}
+	return addFence(notation, result);
+}
+
+
+BinOpNode::BinOpNode(Node* _left, Node* _right, string _operation):
+	operation(_operation), left(_left), right(_right) {}
+BinOpNode::~BinOpNode(){
+	delete left;
+	delete right;
+}
+string BinOpNode::translate(int notation) const{
+	if (operation == "frac" || operation == "sup"){
+		string result;
+		switch (notation) {
+			case MATHML:
+				if (operation == "frac")
+					result = " <mfrac>" + left->translate(notation) +
+							   right->translate(notation) + "</mfrac> ";
+			  if (operation == "sup")
+			  	result = " <msup>" + left->translate(notation) +
+							   right->translate(notation) + "</msup> ";
+				break;
+			case OPENMATH:
+				result = "";
+				break;
+			case TEX:
+				if (operation == "frac")
+					result = "\\frac " + left->translate(notation) + right->translate(notation);
+				if (operation == "sup")
+					result = left->translate(notation) + " ^ " + right->translate(notation);
+				break;
+		}
+		return addFence(notation, result);
+	}
+	else
+		return addFence(notation, left->translate(notation) + createMap()[operation][notation] + right->translate(notation));
+}
+
+
+CompositeNode::CompositeNode(string _lfence = "", string _rfence = ""){
+	setFence(_lfence, _rfence);
+}
+CompositeNode::CompositeNode(Node* _node, string _lfence = "", string _rfence = ""){
+	setFence(_lfence, _rfence);
+	nodes.push_back(_node);
+}
+CompositeNode::CompositeNode(Node* _node){
+	setFence("", "");
+	nodes.push_back(_node);
+}
+CompositeNode::~CompositeNode(){
+	for (int i=0; i<nodes.size(); i++){
+		delete nodes[i];
+	}
+}
+void CompositeNode::add(Node* _node){
+	nodes.push_back(_node);
+}
+string CompositeNode::translate(int notation) const{
+	string s;
+	for (int i=0; i<nodes.size(); i++){
+		 s += nodes[i]->translate(notation) + " ";
+	}
+	s = addFence(notation, s);
+	if (notation == MATHML && Node::lfence != "(")
+		s = "<mrow>" + s + "</mrow>";
+	return s;
+}
--- mathNotationTranslation/mathNotationTranslation/Node.h
+++ mathNotationTranslation/mathNotationTranslation/Node.h
@@ -19,156 +19,51 @@ protected:
 	string rfence;
 public:
 	virtual ~Node(){}
-	virtual string translate(int notation){
-		return string();
-	}
-	static map<string, array<string, 3>> createMap()
-	{
-		map<string, array<string, 3>> m;
-		m["+"] = { " <mo> + </mo> ", "", " + " };
-		m["-"] = { " <mo> - </mo> ", "", " - " };
-		m["*"] = { " <mo> * </mo> ", "", " * " };
-		m["/"] = { " <mo> / </mo> ", "", " / " };
-		m["="] = { " <mo> = </mo> ", "", " = " };
-		return m;
-	}
-	virtual void setFence(string _lfence, string _rfence){
-		lfence = _lfence;
-		rfence = _rfence;
-	}
-	virtual string addFence(int notation, string _s){
-		string s = _s;
-		switch (notation) {
-			case MATHML:
-				if (lfence == "(")
-					s = "<mfenced>" + s + "</mfenced>";
-				break;
-			case OPENMATH:
-				s = "";
-				break;
-			case TEX:
-				s = lfence + s + rfence;
-				break;
-		}
-		return s;
-	}
+	virtual string translate(int notation) const;
+	static map<string, array<string, 3>> createMap();
+	virtual void setFence(string _lfence, string _rfence);
+	virtual string addFence(int notation, string _s) const;
 };
 
 class NumNode : public Node{
-public:
+private:
 	float number;
-	NumNode(float _number): number(_number) {}
-	~NumNode(){}
-	string translate(int notation){
-		string result;
-		string num = string(to_string(number));
-		switch (notation) {
-			case MATHML:
-				result = " <mn> " + num + " </mn> ";
-				break;
-			case OPENMATH:
-				result = num;
-				break;
-			case TEX:
-				result = num;
-				break;
-		}
-		return addFence(notation, result);
-	}
+public:
+	NumNode(float _number);
+	~NumNode();
+	string translate(int notation) const;
 };
 
 class IdNode : public Node{
-public:
+private:
 	string id;
-	IdNode(string _id): id(_id) {}
-	~IdNode(){}
-	string translate(int notation){
-		string result;
-		switch (notation) {
-			case MATHML:
-				result = " <mi> " + id + " </mi> ";
-				break;
-			case OPENMATH:
-				result = id;
-				break;
-			case TEX:
-				result = id;
-				break;
-		}
-		return addFence(notation, result);
-	}
+public:
+	IdNode(string _id);
+	~IdNode();
+	string translate(int notation) const;
 };
 
 class BinOpNode : public Node{
-public:
+private:
 	Node* left;
 	Node* right;
 	string operation;
-
-	BinOpNode(Node* _left, Node* _right, string _operation):
-		operation(_operation), left(_left), right(_right) {}
-	~BinOpNode(){
-		delete left;
-		delete right;
-	}
-	string translate(int notation){
-		if (operation == "frac" || operation == "sup"){
-			string result;
-			switch (notation) {
-				case MATHML:
-					if (operation == "frac")
-						result = " <mfrac>" + left->translate(notation) +
-								   right->translate(notation) + "</mfrac> ";
-				  if (operation == "sup")
-				  	result = " <msup>" + left->translate(notation) +
-								   right->translate(notation) + "</msup> ";
-					break;
-				case OPENMATH:
-					result = "";
-					break;
-				case TEX:
-					if (operation == "frac")
-						result = "\\frac " + left->translate(notation) + right->translate(notation);
-					if (operation == "sup")
-						result = left->translate(notation) + " ^ " + right->translate(notation);
-					break;
-			}
-			return addFence(notation, result);
-		}
-		else
-			return addFence(notation, left->translate(notation) + createMap()[operation][notation] + right->translate(notation));
-	}
+public:
+	BinOpNode(Node* _left, Node* _right, string _operation);
+	~BinOpNode();
+	string translate(int notation) const;
 };
 
 class CompositeNode : public Node{
-public:
+private:
 	vector<Node*> nodes;
-
-	CompositeNode(string _lfence = "", string _rfence = ""){
-		setFence(_lfence, _rfence);
-	}
-	CompositeNode(Node* _node, string _lfence = "", string _rfence = ""){
-		setFence(_lfence, _rfence);
-		nodes.push_back(_node);
-	}
-	~CompositeNode(){
-		for (int i=0; i<nodes.size(); i++){
-			delete nodes[i];
-		}
-	}
-	void add(Node* _node){
-		nodes.push_back(_node);
-	}
-	string translate(int notation){
-		string s;
-		for (int i=0; i<nodes.size(); i++){
-			 s += nodes[i]->translate(notation) + " ";
-		}
-		s = addFence(notation, s);
-		if (notation == MATHML && Node::lfence != "(")
-			s = "<mrow>" + s + "</mrow>";
-		return s;
-	}
+public:
+	CompositeNode(string, string);
+	CompositeNode(Node*);
+	CompositeNode(Node*, string, string);
+	~CompositeNode();
+	void add(Node* _node);
+	string translate(int notation) const;
 };
 
 #endif // NODE_H_INCLUDED
